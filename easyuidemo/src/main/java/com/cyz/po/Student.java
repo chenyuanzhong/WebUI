03804f2c507a94df9f6a294b86344bf66b8924bc
chenyuanzhong <chyzhong2009@126.com>
2013-10-09 23:44:50 +0800

    cyz

---
+++ easyuidemo/src/main/java/com/cyz/po/Student.java
@@ -0,0 +1,13 @@
+package com.cyz.po;
+
+public class Student {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
